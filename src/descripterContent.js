dce953bba5f7cbd9e874b04987f81b85d4bcfa17
engineMaster <xiangshangzhi@gmail.com>
2023-07-31 21:20:26 +0800

    doc: 单文件解析内容

---
+++ src/descripterContent.js
@@ -0,0 +1,133 @@
+/* descipter内容 */
+
+/* 
+ <template>
+  <h1>hello, xiang</h1>
+  <h2>
+    <span>count is {{ count }}</span>
+    <button @click="count++">+1</button>
+  </h2>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "App",
+  setup() {
+    const count = ref(0);
+    return {
+      count,
+    };
+  },
+};
+</script>
+ 
+ */
+/* const json = {
+  filename: 'anonymous.vue',
+  source: '<template>\r\n' +
+    '  <h1>hello, xiang</h1>\r\n' +
+    '  <h2>\r\n' +
+    '    <span>count is {{ count }}</span>\r\n' +
+    '    <button @click="count++">+1</button>\r\n' +
+    '  </h2>\r\n' +
+    '</template>\r\n' +
+    '\r\n' +
+    '<script>\r\n' +
+    'import { ref } from "vue";\r\n' +
+    'export default {\r\n' +
+    '  name: "App",\r\n' +
+    '  setup() {\r\n' +
+    '    const count = ref(0);\r\n' +
+    '    return {\r\n' +
+    '      count,\r\n' +
+    '    };\r\n' +
+    '  },\r\n' +
+    '};\r\n' +
+    '</script>\r\n',
+  template: {
+    type: 'template',
+    content: '\r\n' +
+      '  <h1>hello, xiang</h1>\r\n' +
+      '  <h2>\r\n' +
+      '    <span>count is {{ count }}</span>\r\n' +
+      '    <button @click="count++">+1</button>\r\n' +
+      '  </h2>\r\n',
+    loc: {
+      source: '\r\n' +
+        '  <h1>hello, xiang</h1>\r\n' +
+        '  <h2>\r\n' +
+        '    <span>count is {{ count }}</span>\r\n' +
+        '    <button @click="count++">+1</button>\r\n' +
+        '  </h2>\r\n',
+      start: [Object],
+      end: [Object]
+    },
+    attrs: {},
+    ast: {
+      type: 1,
+      ns: 0,
+      tag: 'template',
+      tagType: 0,
+      props: [],
+      isSelfClosing: false,
+      children: [Array],
+      loc: [Object],
+      codegenNode: undefined
+    },
+    map: {
+      version: 3,
+      sources: [Array],
+      names: [],
+      mappings: ';EACE,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,EAAE,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC;EACpB,CAAC,CAAC,CAAC;IACD,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,EAAE,CAAC,EAAE,CAAC,EAAE,CAAC,CAAC,CAAC,CAAC,EAAE,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC;IAChC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,EAAE,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC;EACrC,CAAC,CAAC,CAAC,CAAC',
+      file: 'anonymous.vue',
+      sourceRoot: '',
+      sourcesContent: [Array]
+    }
+  },
+  script: {
+    type: 'script',
+    content: '\r\n' +
+      'import { ref } from "vue";\r\n' +
+      'export default {\r\n' +
+      '  name: "App",\r\n' +
+      '  setup() {\r\n' +
+      '    const count = ref(0);\r\n' +
+      '    return {\r\n' +
+      '      count,\r\n' +
+      '    };\r\n' +
+      '  },\r\n' +
+      '};\r\n',
+    loc: {
+      source: '\r\n' +
+        'import { ref } from "vue";\r\n' +
+        'export default {\r\n' +
+        '  name: "App",\r\n' +
+        '  setup() {\r\n' +
+        '    const count = ref(0);\r\n' +
+        '    return {\r\n' +
+        '      count,\r\n' +
+        '    };\r\n' +
+        '  },\r\n' +
+        '};\r\n',
+      start: [Object],
+      end: [Object]
+    },
+    attrs: {},
+    map: {
+      version: 3,
+      sources: [Array],
+      names: [],
+      mappings: ';AASA,CAAC,CAAC,CAAC,CAAC,CAAC,EAAE,EAAE,CAAC,CAAC,EAAE,EAAE,CAAC,CAAC,CAAC,EAAE,CAAC,CAAC,CAAC,CAAC,CAAC;AACzB,CAAC,CAAC,CAAC,CAAC,CAAC,EAAE,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,EAAE;EACb,CAAC,CAAC,CAAC,CAAC,EAAE,CAAC,CAAC,CAAC,CAAC,CAAC;EACX,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,EAAE;IACN,CAAC,CAAC,CAAC,CAAC,EAAE,CAAC,CAAC,CAAC,CAAC,EAAE,EAAE,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC;IACpB,CAAC,CAAC,CAAC,CAAC,CAAC,EAAE;MACL,CAAC,CAAC,CAAC,CAAC,CAAC;IACP,CAAC;EACH,CAAC;AACH,CAAC',
+      file: 'anonymous.vue',
+      sourceRoot: '',
+      sourcesContent: [Array]
+    }
+  },
+  scriptSetup: null,
+  styles: [],
+  customBlocks: [],
+  cssVars: [],
+  slotted: false,
+  shouldForceReload: [Function: shouldForceReload]
+} */
